15ab97f989eb5d4263f615b848c755e9713a4e0c
Mikhail Kolesnikov <misha@kmisha.ru>
2019-12-06 14:14:10 +0400

    update the function implementation

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -1,3 +1,11 @@
-const algorithms = number => number > 1 ? number * algorithms(number - 1) : 1
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+const factorialIterable = function*(number) {
+    let i = 1, prev = 1
+    while (i <= number) {
+        prev *= i++
+        yield prev
+    }
+}
 
-export { algorithms }
+
+export { factorial, factorialIterable }
--- src/algorithms/factorial.spec.js
+++ src/algorithms/factorial.spec.js
@@ -1,22 +1,31 @@
-import { algorithms } from './algorithms.js'
+import { factorial, factorialIterable } from './algorithms.js'
 
 describe('The algorithms function', () =>{
     it('call should return correct value', () => {
-        expect(algorithms(0)).toBe(	1);
-        expect(algorithms(1)).toBe(	1);
-        expect(algorithms(2)).toBe(	2);
-        expect(algorithms(3)).toBe(	6);
-        expect(algorithms(4)).toBe(	24);
-        expect(algorithms(5)).toBe(	120);
-        expect(algorithms(6)).toBe(	720);
-        expect(algorithms(7)).toBe(	5040);
-        expect(algorithms(8)).toBe(	40320);
-        expect(algorithms(9)).toBe(	362880);
-        expect(algorithms(10)).toBe(	3628800);
-        expect(algorithms(11)).toBe(	39916800);
-        expect(algorithms(12)).toBe(	479001600);
-        expect(algorithms(13)).toBe(	6227020800);
-        expect(algorithms(14)).toBe(	87178291200);
-        expect(algorithms(15)).toBe(	1307674368000);
+        expect(factorial(0)).toBe(	1);
+        expect(factorial(1)).toBe(	1);
+        expect(factorial(2)).toBe(	2);
+        expect(factorial(3)).toBe(	6);
+        expect(factorial(4)).toBe(	24);
+        expect(factorial(5)).toBe(	120);
+        expect(factorial(6)).toBe(	720);
+        expect(factorial(7)).toBe(	5040);
+        expect(factorial(8)).toBe(	40320);
+        expect(factorial(9)).toBe(	362880);
+        expect(factorial(10)).toBe(	3628800);
+        expect(factorial(11)).toBe(	39916800);
+        expect(factorial(12)).toBe(	479001600);
+        expect(factorial(13)).toBe(	6227020800);
+        expect(factorial(14)).toBe(	87178291200);
+        expect(factorial(15)).toBe(	1307674368000);
+    })
+})
+
+describe('The algorithms generator function', () =>{
+    it('call should return correct value', () => {
+        let res = []
+        for (let i of factorialIterable(10)) res.push(i)
+
+        expect(res).toEqual(	[1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]);
     })
 })
